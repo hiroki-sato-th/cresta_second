fe7071a887391da1459e7e723aaf920206486fa9
hiroki-sato-th-home <hiro3026028@gmail.com>
2022-12-27 07:14:03 +0700

    try commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // コードを貼り付けてください
 $(function(){
     // $('.test').hide();
-    $('.nav_toggle').on('click', function () {
+    // $('.nav_toggle').on('click', function () {
+    //     $('.nav_toggle, .nav').toggleClass('show');
+    // });
+    $('.nav_toggle').click(function(){
         $('.nav_toggle, .nav').toggleClass('show');
     });
+
 }); 
